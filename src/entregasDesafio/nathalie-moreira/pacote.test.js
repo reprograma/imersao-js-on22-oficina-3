78cb1c1338e1da3989d0f48ea2a0d0a4b7e36ce8
NathalieMS <nathalie.moreira@hotmail.com>
2022-08-22 15:22:33 -0300

    feat: introducing Nathalie Moreira answer to code

---
+++ src/entregasDesafio/nathalie-moreira/pacote.test.js
@@ -0,0 +1,29 @@
+const { calcularPacote } = require('../../dominio/calculadora/Projeto/pacote');
+
+describe('Calcular o pacote dado o total de hora do projeto', ()=>{
+    
+    test('Calcular pacote básico', ()=>{
+        const totalHorasProjeto = 50;
+
+        const result = calcularPacote(totalHorasProjeto);
+
+        expect(result).toEqual('pacote_basico');
+    });
+
+    test('Calcular pacote intermediário', ()=>{
+        const totalHorasProjeto = 60;
+
+        const result = calcularPacote(totalHorasProjeto);
+
+        expect(result).toEqual('pacote_intermediario');
+    });
+
+    test('Calcular pacote premium', ()=>{
+        const totalHorasProjeto = 200;
+
+        const result = calcularPacote(totalHorasProjeto);
+
+        expect(result).toEqual('pacote_premium');
+    });
+
+});
