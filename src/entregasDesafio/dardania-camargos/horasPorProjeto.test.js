fe7bd932a9cd6b43aa448511df38ae0e70995e96
dardaniacoimbra <dardaniacoimbra@gmail.com>
2022-08-22 16:30:41 -0300

    Projeto oficina de JS - Reprograma

---
+++ src/entregasDesafio/dardania-camargos/horasPorProjeto.test.js
@@ -0,0 +1,16 @@
+const { calcularHorasDeProjeto } = require('../../dominio/calculadora/Projeto/horasPorProjeto')
+
+describe('Cálculo das horas totais para finalizar um projeto', () => {
+    test('Dado um array com as funcionalidades desejadas no projeto, retorne a soma das horas necessárias', () => {
+        const funcionalidades = [
+            'setup',
+            'construcao_1_pagina',
+            'formulario',
+            'construcao_1_pagina'
+        ]
+        const result = calcularHorasDeProjeto(funcionalidades)
+
+        expect(result).toEqual(40)
+    });
+
+});
